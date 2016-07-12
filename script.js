7b00987762ad720b686dff1135a80aba67adb5e6
David Kerr <kerrd89@gmail.com>
2016-07-12 15:24:04 -0600

    article title and url submit feature

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var title = $('.titleinput');
 var url = $('.urlinput');
 
-
 // add new article to the top of the list
 
-var submit = $('.listcontainer ul').prepend('<li><article><p>Link Title</p><a href>Link URL</a><button>Read</button><button>Remove</button></article></li>');
+var test = $('.submit').on('click', function() {
+  var submit = $('.listcontainer ul').prepend('<li><article><p>'+ $('.titleinput').val()+ '</p><a href>'+ $('.urlinput').val()+ '</a><button>Read</button><button>Remove</button></article></li>');
+});
