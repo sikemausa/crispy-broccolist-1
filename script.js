05422d5371e1a66dec3f009918b0cdb84cd39e3c
David Kerr <kerrd89@gmail.com>
2016-07-12 15:01:32 -0600

    Submit Article JQuery and Comments

---
+++ script.js
@@ -0,0 +1,7 @@
+var title = $('.titleinput');
+var url = $('.urlinput');
+
+
+// add new article to the top of the list
+
+var submit = $('.listcontainer ul').prepend('<li><article><p>Link Title</p><a href>Link URL</a><button>Read</button><button>Remove</button></article></li>');
