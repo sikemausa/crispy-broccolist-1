bb36c3a84abd3e7288b8cfaebd1e7ec250631c7e
David Kerr <kerrd89@gmail.com>
2016-07-12 16:09:17 -0600

    add delete functionality

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var url = $('.urlinput');
 
 // add new article to the top of the list
 
-var test = $('.submit').on('click', function() {
-  var submit = $('.listcontainer ul').prepend('<li><article><p>'+ $('.titleinput').val()+ '</p><a href>'+ $('.urlinput').val()+ '</a><button>Read</button><button>Remove</button></article></li>');
+var submitArticle = $('.submit').on('click', function() {
+  $('.listcontainer ul').prepend('<li><article><p>'+ $('.titleinput').val()+ '</p><a href>'+ $('.urlinput').val()+ '</a><button class = "readToggle">Read</button><button class = "deleteToggle">Remove</button></article></li>');
+});
+
+var markAsRead = $('.readToggle').on('click', function() {
+  $(this).parentsUntil("ul").toggleClass('read');
+});
+
+
+var deleteArticle = $('.deleteToggle').on('click', function() {
+  $(this).parentsUntil("ul").remove();
 });
