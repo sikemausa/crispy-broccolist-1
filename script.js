c9ba52d228d207b26b928a15bf19f57213df0e99
David Kerr <kerrd89@gmail.com>
2016-07-12 19:44:19 -0600

    add counts for articles

--- script.js
+++ script.js
@@ -14,17 +14,26 @@ var submitArticle = $('.submit').on('click', function() {
                                        "<button class ='deleteToggle'>Remove</button>" +
                                      "</article>" +
                                    "</li>");
+    countAll();
   }
 });
 
 var markAsRead = $('.listcontainer ul').on('click', 'button.readToggle',function() {
   $(this).parentsUntil("ul").toggleClass('read');
+  countAll();
 });
 
 var deleteArticle = $('.listcontainer ul').on('click', 'button.deleteToggle' , function() {
   $(this).parentsUntil("ul").remove();
+  countAll();
 });
 
 // var countTotal = $('article').length.on('click', 'button.submitArticle', function () {
 //   $('.totalcount').html("total" + (this));
 // });
+
+function countAll () {
+  $('.totalcount').text($('article').length);
+  $('.totalread').text($('li .read').length);
+  $('.totalunread').text($('article').length - $('li .read').length)
+};
